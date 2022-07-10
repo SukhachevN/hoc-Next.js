f246e89ecb1629c5f6a6f5d3e5fd4bb9f4f2fc04
Nikita <traed.traed@mail.ru>
2022-07-10 23:33:14 +0300

    lesson-9

---
+++ hello-world/pages/product/[productId]/review/[reviewId].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+const Review = () => {
+  const router = useRouter();
+
+  const { reviewId, productId } = router.query;
+
+  return (
+    <h1>
+      Review {reviewId} about product {productId}{' '}
+    </h1>
+  );
+};
+
+export default Review;
